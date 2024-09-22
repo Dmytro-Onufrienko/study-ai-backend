8076f320896995b16dc663c2a06586970026fa1f
Dmytro Onufrienko <DmytroOnufrienko@gmail.com>
2024-09-23 00:36:44 +0300

    Added open ai blank module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { OpenAiModule } from './open-ai/open-ai.module';
 import * as path from 'path';
 
 @Module({
@@ -12,6 +13,7 @@ import * as path from 'path';
       envFilePath: path.resolve(__dirname, '../.env'),
     }),
     AuthModule,
+    OpenAiModule,
   ],
   controllers: [AppController],
   providers: [AppService],
