972cdf45e1d2d68ef38491dc7bc9f291a4e36ca9
Dmytro Onufrienko <DmytroOnufrienko@gmail.com>
2024-10-03 19:09:03 +0300

    .

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
 import { DatabaseService } from 'src/database/database.service';
-import { TopicModule } from 'src/topic/topic.module';
 import { TopicService } from 'src/topic/topic.service';
 
 @Module({
